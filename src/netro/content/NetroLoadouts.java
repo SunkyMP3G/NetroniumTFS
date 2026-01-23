8288eb2168d3c675b249d6af19cb32eafc233e53
Sunky.MPEG <andreymaslow9824@gmail.com>
2026-01-23 21:19:27 +0700

    Loadout bug fixed, map text moved to bundles and a bandaid fix to The Lake enemy base not spawning units

--- src/netro/content/NetroLoadouts.java
+++ src/netro/content/NetroLoadouts.java
@@ -4,7 +4,7 @@ import mindustry.game.*;
 
 public class NetroLoadouts {
     public static Schematic
-            basicHusk;
+        basicHusk;
 
     public static void load(){
         basicHusk = Schematics.readBase64("bXNjaAF4nCXMvQmAMBAG0C9BBLV3C5ewsnECsYjxwGByJ/mpxN1FfAM8aOgKFZtAaEaTnJ1KOtFa4UycZ3NB3w+6nZKN7spOGEDtzUY+QS+rQs+Uo7ArIcg+WIk0HN8BKPxeiIQbKg==");
